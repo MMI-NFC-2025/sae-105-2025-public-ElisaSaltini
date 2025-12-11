327525e8621605a476e934925dd8d31f1063cf21
ElisaSaltini <elisa.saltini@edu.univ-fcomte.fr>
2025-12-11 21:46:55 +0100

    refonte totale

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 
 À compléter :
 
-- NOM :
-- PRÉNOM :
-- GROUPE :
-- URL du site :
-- URL FIGMA :
+- NOM : SALTINI
+- PRÉNOM : Elisa
+- GROUPE : B1
+- URL du site : 
+- URL FIGMA : https://www.figma.com/design/zk7BAVIepTM9uVsTiyg9Iz/SAE105?node-id=23-476&t=mCtIFDNm0zbBSJI3-1
 
 ## Sujet
 
@@ -16,4 +16,4 @@
 
 Je vous informe ici que mon compte étudiant Infomaniak a été refusé pour cause de carte d'identité invalide. Tout les document concernant ce désagrément ont été envoyés à monsieur Pracht via la boite mail universitaire. Merci de bien vouloir prendre en compte ce refus lors de ma notation.
 
-Il y a moins de push et de commits que prevus parce que j'ai commencé le developpement dans un autre fichier.
+Il y a moins de push et de commits que prevus parce que j'ai commencé le developpement dans un autre fichier. Vous pouvez retrouver les traces de cela dans mon compte GITHUB. 
--- src/js/partners-carousel.js
+++ src/js/partners-carousel.js
@@ -1,20 +1,20 @@
 // CARROUSEL PARTENAIRES 
 
 document.addEventListener("DOMContentLoaded", () => {
+    const carousel = document.querySelector(".partners__carousel");
+    if (!carousel) return;
 
-    const img = document.querySelector(".partners-carousel .carousel__image img");
-    if (!img) return;
+    const img = carousel.querySelector(".partners__image");
+    const prev = carousel.querySelector(".partners__control--prev");
+    const next = carousel.querySelector(".partners__control--next");
+    if (!img || !prev || !next) return;
 
     const images = img.dataset.images.split(",").map(src => src.trim());
-
-    const prev = document.querySelector(".partners-carousel .carousel__btn--prev");
-    const next = document.querySelector(".partners-carousel .carousel__btn--next");
-
     let index = 0;
 
-    function update() {
+    const update = () => {
         img.src = images[index];
-    }
+    };
 
     next.addEventListener("click", () => {
         index = (index + 1) % images.length;
@@ -25,7 +25,6 @@ document.addEventListener("DOMContentLoaded", () => {
         index = (index - 1 + images.length) % images.length;
         update();
     });
-
 });
 
 
